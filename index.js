a12bae83f3a7163fd405e6012cdd5dfa676628a5
Roxana <rservin5@cps.edu>
2018-02-14 20:36:59 +0000

    add todo add and js file

--- index.js
+++ index.js
@@ -14,3 +14,11 @@ $(document).ready(function() {
    pomodoroEstimate.val("")
  })
 })
+
+
+$(document).ready(function() {
+  // Strike through todo when its checkbox is clicked
+  $("ul").on("click", "input[type=checkbox]", function(){
+    $(this).closest("li").toggleClass("completed-todo")
+  })
+})
